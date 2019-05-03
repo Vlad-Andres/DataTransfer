2916b69e3afd06fe7d5f2c8dd02375654205617b
Vlad-Andres <bruno993409@gmail.com>
2019-05-03 10:41:45 +0300

    first commit

---
+++ dataTransferCore/src/main/java/com/doxbit/dataTransfer/App.java
@@ -0,0 +1,13 @@
+package com.doxbit.dataTransfer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
