1ba569100eac9c47cfd6124a687f595c486fa4f0
Vlad-Andres <bruno993409@gmail.com>
2019-05-07 11:58:00 +0300

    Working String MVC with controller

---
+++ dataTransferWeb/src/main/java/com/doxbit/dataTransfer/TestController.java
@@ -0,0 +1,24 @@
+package com.doxbit.dataTransfer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+	
+	String message = "Welcome to Spring MVC AND FROM WA WA <h1>WAR</h1>!";
+	
+	@RequestMapping("/test")
+	public ModelAndView helloWorld(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name ) 
+	{
+	
+		ModelAndView mv = new ModelAndView("test");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+
+}
--- dataTransferWeb/src/main/webapp/WEB-INF/jsp/test.jsp
+++ dataTransferWeb/src/main/webapp/WEB-INF/jsp/test.jsp
@@ -3,10 +3,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Insert title here</title>
 </head>
 <body>
-
+${message}
 </body>
 </html>
